b3092ad406803f2edac0c8e8b03393e9e7351605
bryan_perez <bam.perez@yavirac.edu.ec>
2020-09-17 16:20:20 -0500

    Quito líneas innecesarias que estorban

--- problema2.js
+++ problema2.js
@@ -9,34 +9,18 @@
     }
 
 }*/
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 //descripcion
 //el programa muestra los 20 primeros numeros de la serie de fibonacci,
 function fibonacci()
-=======
 
-function fibonacci()
->>>>>>> cabecera
-=======
-
-function fibonacci()
->>>>>>> cabecera
     {
         let fibo=[0,1];
         for (let i = 2; i <= 19; i++) {
             fibo.push(fibo[i - 2] + fibo[i - 1]);
-        }
-<<<<<<< HEAD
-<<<<<<< HEAD
-        return fibo;
-=======
-        return fibo;
-    
->>>>>>> cabecera
-=======
-        return fibo;
-    
->>>>>>> cabecera
+        }    
+
+        return fibo;    
+
     }
 console.log(fibonacci());
