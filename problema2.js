55d3b9d3600daf37d8c4ce815aa2db06475c3f0b
bryan_perez <bam.perez@yavirac.edu.ec>
2020-09-16 19:22:57 -0500

    al js problema2, se quita parametro y se agrega un consolelog

--- problema2.js
+++ problema2.js
@@ -10,11 +10,13 @@
 
 }*/
 
-function fibonacci(numero)
+function fibonacci()
     {
         let fibo=[0,1];
         for (let i = 2; i <= 19; i++) {
             fibo.push(fibo[i - 2] + fibo[i - 1]);
         }
         return fibo;
+    
     }
+console.log(fibonacci());
