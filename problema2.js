4a33a8ada2ca6c090909cc51a1f78bb69fe49039
bryan_perez <bam.perez@yavirac.edu.ec>
2020-09-16 19:17:16 -0500

    Agregué una descripción al js problema2

--- problema2.js
+++ problema2.js
@@ -9,12 +9,13 @@
     }
 
 }*/
-
+//descripcion
+//el programa muestra los 20 primeros numeros de la serie de fibonacci,
 function fibonacci(numero)
     {
         let numeros=[0,1];
         for (let i = 2; i < numero; i++) {
             numeros[i] = numeros[i - 2] + numeros[i - 1];
         }
-        return numeros;
+        return numros;
     }
