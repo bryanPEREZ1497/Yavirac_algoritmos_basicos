625e5fb7ed9a1adcf4f14c838cdcfaec4a7c72b2
bryan_perez <bam.perez@yavirac.edu.ec>
2020-12-05 15:11:19 -0500

    PLusONE

---
+++ plusOne.js
@@ -0,0 +1,15 @@
+const plusOne = digits => {
+  for (let i = digits.length-1; i >=0;  i--) {
+    if (digits[i]!=9) {
+      digits[i]++;
+      return digits;
+    }else {
+      digits[i]=0;
+    }
+  }
+  digits.unshift(1);
+  return digits;
+}
+
+digits = [9,9,9]
+console.log(`${digits} to ${plusOne(digits)}`);
